7db1c8ddac9d822910493d8c7aec67a8389bb715
Leslie Meng <mengli26@gmail.com>
2021-01-24 02:44:03 -0500

    BE: started seed file

--- Server/api/message.js
+++ Server/api/message.js
@@ -1,17 +1,19 @@
-// const router = require('express').Router()
-// const {Message, User} = require('../db');
-// const channel = require('../db/Models/channel');
+const router = require('express').Router()
+const {Message, User} = require('../db/Models')
+const channel = require('../db/Models/channel')
 
-// module.exports = router;
+//GET api/messages
+router.get('/', async (req, res, next) => {
+  try {
+    const messages = await Message.findAll({
+      //   where: {channelId: req.body.channelId},
+      //   include: {model: User},
+    })
+    res.json(messages)
+  } catch (err) {
+    next(err)
+  }
+})
 
-// //GET api/messages
-// router.get('/', async (req, res, next) => {
-//     try {
-//         const messages = await Message.findAll({where: {channelId: req.body.channelId}, include: { model: User, attributes: [Name]}})
-//         res.json(messages)
-//     } catch (err) {
-//         next(err)
-//     }
-// })
-
-// //POST /api/
+//POST /api/
+module.exports = router
