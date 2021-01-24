bf4f7f2abc81831011f92f57016d8061b7c1b59b
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-01-24 01:22:42 -0600

    FE: Added redux channel

--- Client/store/index.js
+++ Client/store/index.js
@@ -1,11 +1,13 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import channelReducer from './channel'
 import messageReducer from './messages'
 import user from './user'
 
 const reducer = combineReducers({
   user,
   messages: messageReducer,
+  channel: channelReducer,
 })
 
 const middleware = applyMiddleware(thunkMiddleware)
