d4a5e2ca185c9254b9921a2c222030f4d1e021a4
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-01-24 01:10:12 -0600

    FE: set up message redux

--- Client/store/index.js
+++ Client/store/index.js
@@ -1,9 +1,11 @@
 import {createStore, applyMiddleware, combineReducers} from 'redux'
 import thunkMiddleware from 'redux-thunk'
+import messageReducer from './messages'
 import user from './user'
 
 const reducer = combineReducers({
   user,
+  messages: messageReducer,
 })
 
 const middleware = applyMiddleware(thunkMiddleware)
