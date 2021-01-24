3975a3bdb6345bdefd90dd8532daa6f3ea161fa1
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-01-23 21:43:21 -0600

    BE: Added a user route to get thier channel list and infomation

--- Server/api/index.js
+++ Server/api/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
-module.exports = router
 
 router.use('/user', require('./user'))
+
+module.exports = router
--- Server/api/user.js
+++ Server/api/user.js
@@ -1,4 +1,34 @@
-const router = require(express).Router()
-const {User} = require('../db')
+const router = require('express').Router()
+const {User} = require('../db/Models')
+const Channel = require('../db/Models/channel')
 
 module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const userId = req.session.passport.user
+    if (!userId) {
+      const error = new Error('User not logged in')
+      error.status = 404
+      throw error
+    }
+    const info = await User.findbyPK(userId, {
+      attibutes: ['Name', 'Interest', 'Image', 'TimeZone'],
+      include: Channel,
+    })
+    console.log(info)
+    if (!info) {
+      const error = new Error('User not found')
+      error.status = 404
+      throw error
+    } else if (!info.Channel) {
+      const error = new Error('User has no channels')
+      error.status = 404
+      throw error
+    } else {
+      res.send(info)
+    }
+  } catch (err) {
+    next(err)
+  }
+})
