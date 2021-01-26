259b65701f32534f2edf99960a8364999628d6ee
Leslie Meng <mengli26@gmail.com>
2021-01-26 18:47:56 -0500

    test

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const isDev = process.env.NODE_ENV === 'development'
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
+  context: __dirname + '/client',
   entry: [
     '@babel/polyfill', // enables async-await
     './client/index.js',
